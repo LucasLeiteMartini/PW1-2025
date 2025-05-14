d493ff4d375cfd04e51973628d5c52a911d3184f
Lucas Martini <lucas.leite.martini@gmail.com>
2025-05-13 20:37:06 -0400

    implementação de parada de movimento, start e pause do jogo e toggle de menu

--- game/js/game.js
+++ game/js/game.js
@@ -2,22 +2,81 @@ import {FPS} from "./config.js";
 import {space} from "./space.js";
 import {ship} from "./ship.js";
 import {createEnemyShip, moveEnemyShip} from "./enemyShip.js";
+import {createShot, moveShot} from "./shipLaser.js";
+
+//Vairáveis para controle de movimento
+let leftPressed = false;
+let rightPressed = false;
+
+//Variáveis para controle de pause
+let isPaused = false;
+let gameRunning = false
+let gameState
+
+//Variável para controle de nível
+let level = 1
 
 function init(){
-    setInterval(run, FPS)
+    gameState = setInterval(run, FPS)
+}
+
+function pause(){
+    clearInterval(gameState);
 }
 
 function run(){
     space.move()
-    console.log("oi")
-    ship.move()
     createEnemyShip()
-    moveEnemyShip()
+    moveEnemyShip(15)
+    moveShot()
 }
 
+// Responsável por iniciar o jogo
+window.addEventListener("keyup", (e)=>{
+    if(e.key === " " && !gameRunning) {
+        gameRunning = true
+        init()
+        document.getElementById("menu").style.display = "none"; //Oculta o menu inicial do jogo.
+    }
+})
+
+
+
+// Responsável pela animação de movimento da nave
 window.addEventListener("keydown", (e)=>{
-    if(e.key === "ArrowLeft") ship.changeDirection(-1)
-    if(e.key === "ArrowRight") ship.changeDirection(+1)
+    if(e.key === "ArrowRight") rightPressed = true
+    if(e.key === "ArrowLeft") leftPressed = true
 });
 
-init()
+// Responsável pela animação de movimento da nave
+window.addEventListener("keyup", (e)=>{
+    if(e.key === "ArrowRight") rightPressed = false
+    if(e.key === "ArrowLeft") leftPressed = false
+})
+
+
+// Responsável pela animação de movimento da nave
+setInterval(()=>{
+    if(rightPressed && gameRunning && !isPaused) {
+        ship.changeDirection(1)
+    }else if(leftPressed && gameRunning && !isPaused) {
+        ship.changeDirection(-1)
+    }else{
+        ship.direction = 1
+    }
+    ship.move()
+
+}, 5)
+
+// Pausa e despausa o game, fazendo toggle do menu
+window.addEventListener("keypress", (e)=>{
+    if(e.key === "p" && isPaused){
+        isPaused = false
+        document.getElementById("menu").style.display = "none";
+        init()
+    }else if(e.key === "p" && !isPaused){
+        isPaused = true
+        document.getElementById("menu").style.display = "block";
+        pause()
+    }
+})
