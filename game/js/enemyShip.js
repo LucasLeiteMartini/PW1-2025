b69083ad5d4ecf71ab73dd18e5ac13a4cb3a5284
Lucas Martini <lucas.leite.martini@gmail.com>
2025-05-21 01:01:22 -0400

    Novo atributo speed para atender a regra 5. Calculo da posição X da nave inimiga melhorado.

--- game/js/enemyShip.js
+++ game/js/enemyShip.js
@@ -2,26 +2,42 @@ import {PROB_ENEMY_SHIP, TAM_X} from "./config.js";
 import {space} from "./space.js";
 
 class EnemyShip {
-    constructor(){
+    constructor(speed){
         this.element = document.createElement("img");
         this.element.className = "enemy-ship"
         this.element.src = "assets/png/enemyShip.png"
         this.element.style.top = "-20px"
-        this.element.style.left = `${parseInt(Math.random() * TAM_X)}px`
+        this.element.style.left = `${randomPosition()}px`
+        this.speed = speed;
         space.element.appendChild(this.element)
     }
 
-    move(speed){
-        this.element.style.top = `${parseInt(this.element.style.top) + speed}px`
+    move(){
+        this.element.style.top = `${parseInt(this.element.style.top) + this.speed}px`
     }
 }
 
-const enemyShip = []
+export const enemyShips = []
 
-export const createEnemyShip = () => {
-    if(Math.random() < PROB_ENEMY_SHIP) enemyShip.push(new EnemyShip())
+export const createEnemyShip = (speed) => {
+    if(Math.random() < 0.04) enemyShips.push(new EnemyShip(speed))
 }
 
-export const moveEnemyShip = (speed) => {
-    enemyShip.forEach(e => e.move(speed))
+/**
+ * Posição aleatória gerada para a nave inimiga. É gerada novamente se estiver fora dos limites.
+ */
+function randomPosition(){
+    let pos = Math.random() * TAM_X;
+    while(pos > 502 || pos < 0){
+        pos = Math.random() * TAM_X;
+    }
+    return pos;
 }
+
+/**
+ * Faz um loop que move individualmente cada nave dentro do array.
+*/
+export const moveEnemyShip = () => {
+    enemyShips.forEach(e => e.move())
+}
+
