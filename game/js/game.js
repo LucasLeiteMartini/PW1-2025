86cbbb9191a1ea869e2a202260cf755d44d3f67a
Lucas Martini <lucas.leite.martini@gmail.com>
2025-05-21 01:00:07 -0400

    Adição da função checkOutOfBounds. Controle de cadência do tiro da nave. Melhora no eventListener de quando se inicia o jogo para não haver conflito com o eventListener do tiro. Melhora nos comentários da aplicação.

--- game/js/game.js
+++ game/js/game.js
@@ -1,61 +1,78 @@
 import {FPS} from "./config.js";
 import {space} from "./space.js";
 import {ship} from "./ship.js";
-import {createEnemyShip, moveEnemyShip} from "./enemyShip.js";
-import {createShot, moveShot} from "./shipLaser.js";
+import {createEnemyShip, enemyShips, moveEnemyShip} from "./enemyShip.js";
+import {createShot, moveShot, shots} from "./shipLaser.js";
 
-//Vairáveis para controle de movimento
+//Vairáveis para controle de ações
 let leftPressed = false;
 let rightPressed = false;
+let shotPressed = false;
 
 //Variáveis para controle de pause
 let isPaused = false;
 let gameRunning = false
 let gameState
 
-//Variável para controle de nível
-let level = 1
+//Variável para controle de velocidade
+let speed = 2
 
+// Variável para controle de cadência de tiro
+let shotCooldown = 0
+
+/**
+ * Função chamada para começar o jogo.
+ * Guarda o retorno de setInterval para que seja possível manejar o pausar/despausar do jogo
+ */
 function init(){
     gameState = setInterval(run, FPS)
 }
 
+/** Função chamada quando o jogo é pausado. */
 function pause(){
     clearInterval(gameState);
 }
 
+/** Chamada dentro de init, é responsável por manter as animações de movimento não controladas pelo jogador.
+ * Responsável também por chamar outras funções como checkCollision e checkOutOfBounds
+ */
 function run(){
     space.move()
-    createEnemyShip()
-    moveEnemyShip(15)
+    createEnemyShip(Math.floor(Math.random() * 3) + 1)
+    moveEnemyShip();
     moveShot()
+    checkOutOfBounds()
 }
 
 // Responsável por iniciar o jogo
-window.addEventListener("keyup", (e)=>{
-    if(e.key === " " && !gameRunning) {
+function start(e){
+    if(e.key === " " && !gameRunning){
         gameRunning = true
         init()
-        document.getElementById("menu").style.display = "none"; //Oculta o menu inicial do jogo.
+        document.getElementById("menu").style.display = "none"
+        window.removeEventListener('keyup', start)
     }
-})
+}
+
+window.addEventListener('keyup', start)
 
 
-
-// Responsável pela animação de movimento da nave
+// Responsável por alternar movimento/tiro da nave
 window.addEventListener("keydown", (e)=>{
     if(e.key === "ArrowRight") rightPressed = true
     if(e.key === "ArrowLeft") leftPressed = true
+    if(e.key === " ") shotPressed = true
 });
 
-// Responsável pela animação de movimento da nave
+// Responsável por alternar movimento/tiro da nave
 window.addEventListener("keyup", (e)=>{
     if(e.key === "ArrowRight") rightPressed = false
     if(e.key === "ArrowLeft") leftPressed = false
+    if(e.key === " ") shotPressed = false
 })
 
 
-// Responsável pela animação de movimento da nave
+// Responsável por alternar movimento/tiro da nave
 setInterval(()=>{
     if(rightPressed && gameRunning && !isPaused) {
         ship.changeDirection(1)
@@ -66,17 +83,45 @@ setInterval(()=>{
     }
     ship.move()
 
-}, 5)
+    if(shotPressed && gameRunning && !isPaused) {
+        shotCooldown++
+        if(shotCooldown % 15 === 0) {
+            createShot()
+        }
+    }
+
+}, FPS)
 
 // Pausa e despausa o game, fazendo toggle do menu
 window.addEventListener("keypress", (e)=>{
-    if(e.key === "p" && isPaused){
+    if(e.key === "p" && isPaused && gameRunning) {
         isPaused = false
         document.getElementById("menu").style.display = "none";
         init()
-    }else if(e.key === "p" && !isPaused){
+    }else if(e.key === "p" && !isPaused && gameRunning) {
         isPaused = true
         document.getElementById("menu").style.display = "block";
         pause()
     }
 })
+
+
+function checkCollision(obj1, obj2) {
+
+}
+
+function checkOutOfBounds(){
+    shots.forEach((shot,shotIndex) => {
+        if(parseInt(shot.element.style.bottom) > 900){
+            shot.element.remove()
+            shots.splice(shotIndex,1);
+        }
+    })
+
+    enemyShips.forEach((enemyShip, enemyShipIndex)=>{
+        if(parseInt(enemyShip.element.style.top) > 900){
+            enemyShip.element.remove()
+            enemyShips.splice(enemyShipIndex, 1)
+        }
+    })
+}
