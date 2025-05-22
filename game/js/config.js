a760e7df21e202a5c856f7355c4238368ee3971e
Martini <lucas.leite.martini@gmail.com>
2025-05-22 07:53:20 -0400

    adição dos objetos points e enemyProbability para uma gestão mais facil

--- game/js/config.js
+++ game/js/config.js
@@ -2,4 +2,16 @@ export const FPS = 1000 / 60
 export const TAM_X = 600
 export const TAM_Y = 900
 
-export const PROB_ENEMY_SHIP = 0.02
+export const enemyProbability = {
+    bigMeteor: 0.03,
+    ufo: 0.025,
+    enemyShip: 0.009,
+    smallMeteor: 0.005,
+}
+
+export const points = {
+    bigMeteor: 10,
+    ufo: 20,
+    enemyShip: 50,
+    smallMeteor: 100
+}
